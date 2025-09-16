23f71551cd2cf6767e54061c40aa43bf63736565
{Rohit Bej} <{rohit098bej@gmail.com}>
2025-09-17 02:12:02 +0530

    added pages

---
+++ src/pages/Publish.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function Publish() {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Publish Page</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
