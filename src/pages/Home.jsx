e6cb054f3d9a8199d3eab5b11e7d67df4ceaa293
Sumit Kumar Manjhi <sumitmanjhi923@gmail.com>
2025-09-21 21:49:06 +0530

    changed file name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import FeaturesSection from '../components/FeaturesSection';
 import ScheduleSection from '../components/ScheduleSection';
 import MonitorSection from '../components/MonitorSection';
 import PricingSection from '../components/PricingSection';
-import Oportunities from '../components/Oportunities';
+import Oportunities from '../components/Opportunities';
 import TestimonialsSection from '../components/TestimonialsSection';
 import NewsletterSection from '../components/NewsletterSection';
 import Footer from '../components/Footer';
